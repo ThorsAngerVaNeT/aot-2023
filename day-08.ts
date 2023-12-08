e62880b612046a244f6679c7ffabf99ab5f8626f
Vadzim Antonau <vanet@tut.by>
2023-12-08 08:15:59 +0000

    feat: update day 8 challenge solution

--- day-08.ts
+++ day-08.ts
@@ -1,3 +1,1 @@
-type FilterNotStartingWith<Set, Needle extends string> = Set extends `${Needle}${infer _X}` ? never : Set
-
-type RemoveNaughtyChildren<List extends Record<string, unknown>> = Pick<List, FilterNotStartingWith<keyof List, 'naughty'>>
+type RemoveNaughtyChildren<List> = Omit<List, `naughty_${string}`>
