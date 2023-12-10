ef1f3a65e9cd98b9f1520dbba48b1be1a8af6f4b
Vadzim Antonau <vanet@tut.by>
2023-12-10 09:15:47 +0000

    feat: update day 9 challenge solution

--- day-09.ts
+++ day-09.ts
@@ -1,17 +1,1 @@
-type Split<S extends string> = S extends ''
-  ? []
-  : S extends `${infer C}${infer R}`
-  ? [C, ...Split<R>]
-  : []
-
-type Join<T extends string[]> = T extends []
-  ? ''
-  : T extends [infer Head, ...infer Tail]
-  ? Head extends string
-  ? `${Head}${Join<Tail extends string[] ? Tail : []>}`
-  : never
-  : never
-
-type ReverseArray<T> = T extends [...infer H, infer T] ? [T, ...ReverseArray<H>] : [];
-
-type Reverse<T extends string> = Join<ReverseArray<Split<T>>>
+type Reverse<T extends string> = T extends `${infer First}${infer Rest}` ? `${Reverse<Rest>}${First}` : '';
