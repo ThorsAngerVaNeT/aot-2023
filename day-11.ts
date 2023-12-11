a5389de8284978f2ae5f1e2773137d64bab09f24
Vadzim Antonau <vanet@tut.by>
2023-12-11 05:34:27 +0000

    feat: add day 11 challenge solution

---
+++ day-11.ts
@@ -0,0 +1,3 @@
+type SantaListProtector<Type> = {
+  readonly [Property in keyof Type]: Type[Property] extends Record<string, unknown> | unknown[] ? SantaListProtector<Type[Property]> : Type[Property];
+}
