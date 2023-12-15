c36f9007914f4112fc4b90789698e56021a5ac20
Vadzim Antonau <vanet@tut.by>
2023-12-15 05:47:06 +0000

    feat: update day 15 challenge solution

--- day-15.ts
+++ day-15.ts
@@ -1,12 +1,3 @@
-type ArrayOfItems<Item extends string, Count extends number, Result extends string[] = []> =
-  Result['length'] extends Count
+type BoxToys<Item extends string, Count extends number, Result extends string[] = []> = Count extends Result['length']
   ? Result
-  : ArrayOfItems<Item, Count, [...Result, Item]>;
-
-type BoxToys<Item extends string, Count extends number> = O2T<{
-  [key in Count]: ArrayOfItems<Item, key>
-}>;
-
-type O2T<O extends Record<string, any>> = {} extends O ? [] : {
-  [key in keyof O]: O[key];
-}[keyof O]
+  : BoxToys<Item, Count, [...Result, Item]>;
