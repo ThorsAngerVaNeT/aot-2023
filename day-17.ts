69ba00cfa2906403909e7a6d4b69e5ddcad73410
Vadzim Antonau <vanet@tut.by>
2023-12-17 07:40:33 +0000

    feat: update day 17 challenge solution

--- day-17.ts
+++ day-17.ts
@@ -1,18 +1,14 @@
 type RockPaperScissors = '👊🏻' | '🖐🏾' | '✌🏽';
 
+type WinPair = {
+  '👊🏻': '🖐🏾',
+  '🖐🏾': '✌🏽',
+  '✌🏽': '👊🏻'
+}
+
 type WhoWins<Player1 extends RockPaperScissors, Player2 extends RockPaperScissors> =
   Player1 extends Player2
   ? 'draw'
-  : Player1 extends '👊🏻'
-    ? Player2 extends '🖐🏾'
-      ? 'win'
-      : 'lose'
-    : Player1 extends '🖐🏾'
-      ? Player2 extends '👊🏻'
-        ? 'lose'
-        : 'win'
-      : Player1 extends '✌🏽'
-        ? Player2 extends '👊🏻'
-          ? 'win'
-          : 'lose'
-        : 'draw';
+  : Player1 extends WinPair[Player2]
+    ? 'lose'
+    : 'win';
