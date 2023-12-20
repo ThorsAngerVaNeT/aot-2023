27f5078cda5deb4d465ee39b05952f4b3f9dc60b
Vadzim Antonau <vanet@tut.by>
2023-12-20 05:41:08 +0000

    feat: update day 20 challenge solution

--- day-20.ts
+++ day-20.ts
@@ -52,34 +52,21 @@ type Letters = {
         "░"];
 };
 
-type ConcatAsciiArtParts<PartOne extends string[], PartTwo extends string[]> = PartOne extends [
-	infer FirstCharOne extends string,
-	infer SecondCharOne extends string,
-	infer ThirdCharOne extends string,
-]
-	? PartTwo extends [
-			infer FirstCharTwo extends string,
-			infer SecondCharTwo extends string,
-			infer ThirdCharTwo extends string,
-		]
-		? [
-				`${FirstCharOne}${FirstCharTwo}`,
-				`${SecondCharOne}${SecondCharTwo}`,
-				`${ThirdCharOne}${ThirdCharTwo}`,
-			]
-		: never
-	: never;
+type ConcatAsciiArtParts<
+  PartOne extends [string, string, string],
+  PartTwo extends [string, string, string],
+> = [`${PartOne[0]}${PartTwo[0]}`, `${PartOne[1]}${PartTwo[1]}`, `${PartOne[2]}${PartTwo[2]}`];
 
 type LineToAsciiArt<
-	Line extends string,
-	Result extends [string, string, string] = ["", "", ""],
+  Line extends string,
+  Result extends [string, string, string] = ["", "", ""],
 > = Uppercase<Line> extends `${infer First extends keyof Letters}${infer Rest}`
-	? LineToAsciiArt<Rest, ConcatAsciiArtParts<Result, Letters[First]>>
-	: Result;
+  ? LineToAsciiArt<Rest, ConcatAsciiArtParts<Result, Letters[First]>>
+  : Result;
 
 type ToAsciiArt<
-	Message extends string,
-	Result extends string[] = [],
+  Message extends string,
+  Result extends string[] = [],
 > = Message extends `${infer FirstLine}\n${infer RestLines}`
-	? ToAsciiArt<RestLines, [...Result, ...LineToAsciiArt<FirstLine>]>
-	: [...Result, ...LineToAsciiArt<Message>];
+  ? ToAsciiArt<RestLines, [...Result, ...LineToAsciiArt<FirstLine>]>
+  : [...Result, ...LineToAsciiArt<Message>];
